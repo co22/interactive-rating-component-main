e93b07ed4c13e81dc0b72820941a5f78eaa3fae1
co22 <co22@up.edu>
2022-09-29 02:52:53 -0700

    Completed form/button functionality.

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ window.onload = () => {
  */
 function setEventListeners() {
     const submitButton = document.querySelector(".submit-button");
+    const radioButtons = document.querySelectorAll("input");
+
     submitButton.addEventListener("click", hideRatingState);
     submitButton.addEventListener("click", displayThankYouState);
+    submitButton.addEventListener("click", setRating);
+
+    for (const button of radioButtons) {
+        button.addEventListener("click", changeActiveButtonColor);
+    }
 }
 
 /** 
@@ -34,5 +41,45 @@ function displayThankYouState() {
     const thankYouStateContainer = document.querySelector(".thank-you-state-container");
     thankYouStateContainer.style.display = "block";
     thankYouStateContainer.style.visibility = "visible";
+    return false;
+}
+
+/**
+ * Change color of rating buttons after clicked.
+ * @param: NA
+ * @return: NA
+ */
+function changeActiveButtonColor() {
+    const radioLabels = document.getElementsByTagName("label");
+    const radioButtons = document.getElementsByTagName("input"); 
+
+    for (let i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+            radioLabels[i].style.color = "var(--white)";
+            radioLabels[i].style.background = "var(--orange)";
+        }
+        else {
+            radioLabels[i].style.color = "var(--light-grey)";
+            radioLabels[i].style.background = "var(--modified-dark-blue)";
+        }
+    }
+}
+
+/**
+ * Change result text to show user-selected rating.
+ * @param: NA
+ * @return: NA
+ */
+function setRating() {
+    const radioButtons = document.getElementsByTagName("input");
+    const ratingResultText = document.querySelector(".rating-result");
+    let ratingResult = "";
+
+    for (const button of radioButtons) {
+        if (button.checked) {
+            ratingResult = "You selected " + button.value + " out of 5";
+        }
+    }
+    ratingResultText.innerHTML = ratingResult.toString();
     return false;
 }
